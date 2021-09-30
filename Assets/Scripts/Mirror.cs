25acefbff33b80a1f7ba928f2b838c127858d654
Aaron <aaronkingnc@att.net>
2021-09-29 21:02:05 -0400

    Fixed mirror rotation

--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -8,48 +8,29 @@ using static UnityEngine.InputSystem.InputAction;
 public class Mirror : MonoBehaviour
 {
     public float rotationRadius;
-    public float maxAngle;
-    public float minAngle;
     Vector2 unitVector;
     public Transform parentPos;
-    bool updateRotation;
     bool rotateMirrorLocally = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        
-
         CalculatePosition();
-        if (updateRotation)
-        {
-            transform.localPosition = rotationRadius * unitVector;
-            transform.up = parentPos.position - transform.position;
-            //transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, 0);
-        }
+        transform.localPosition = rotationRadius * unitVector;
+        transform.up = parentPos.position - transform.position;
     }
     void CalculatePosition()
     {
-        updateRotation = false;
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        Vector2 worldPos2 = new Vector2(worldPos.x, worldPos.y);
+        Vector2 mousePos = new Vector2(worldPos.x, worldPos.y);
 
         Vector2 parentPos2;
         if (rotateMirrorLocally) {
             parentPos2 = new Vector2(parentPos.position.x, parentPos.position.y);
         }
         else {
-            parentPos2 = new Vector2(transform.position.x, transform.position.y);
+            parentPos2 = new Vector2(parentPos.position.x, parentPos.position.y);
         }
-        if (Vector2.Angle(parentPos2, worldPos2) > minAngle)
-        {
-            unitVector = (worldPos2 - parentPos2).normalized;
-            updateRotation = true;
-        }
+        unitVector = (mousePos - parentPos2).normalized;
     }
 }
